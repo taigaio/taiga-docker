8f6cf613016deb686a6fdd9caddbdf440ee7a0e6
Ralf Yang <goody80762@gmail.com>
2022-06-02 10:48:40 +0900

    Update taiga-manage.sh
    Change the command for docker new version(docker-compose has been included)

--- taiga-manage.sh
+++ taiga-manage.sh
@@ -7,4 +7,4 @@
 # Copyright (c) 2021-present Kaleidos Ventures SL
 
 set -x
-exec docker-compose -f docker-compose.yml -f docker-compose-inits.yml run --rm taiga-manage $@
+exec docker compose -f docker-compose.yml -f docker-compose-inits.yml run --rm taiga-manage $@
