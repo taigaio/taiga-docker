70e521dfb6b03721e39935eb1000308bc88a65b4
Ani peter benjamin <99623072+vortex-hue@users.noreply.github.com>
2024-01-24 02:58:36 +0100

    compatible with docker compose v2.x

---
+++ launch-all-v2.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2021-present Kaleidos Ventures SL
+
+set -x
+exec docker compose -f docker-compose.yml -f docker-compose.penpot.yml up -d $@
